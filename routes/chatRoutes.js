58490098879b0199c2311125bc45552fbd6e7033
hemanthkumarsangam <hamanth4k@gmail.com>
2024-12-30 21:22:03 +0530

    chat routes updated

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,9 +2,15 @@ const Chats = require('../models/chatModel');
 const express = require('express');
 const updtChat = express.Router();
 
-updtChat.get('/getChat/:id', async (req, res) => {
+updtChat.patch('/:id', async (req, res) => {
   const chatId = req.params.id;
   const result = await Chats.findOne({chatId})
+  if(result === null){
+    const chat = new Chats({chatId, chat : []})
+    await chat.save()
+    res.send(chat);
+    return;
+  }
   res.send(result)
 })
 
